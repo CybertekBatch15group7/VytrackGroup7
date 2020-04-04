7ffa00b5a9ff7d164a2e80dda43ef504c7dda5d8
akbalsedat <akbalsedat@gmail.com>
2020-04-04 00:53:28 -0400

    print statement changed to "Hello Group-7"

--- src/test/java/com/TestFile.java
+++ src/test/java/com/TestFile.java
@@ -3,6 +3,6 @@ package com;
 public class TestFile {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Group-7");
     }
 }
